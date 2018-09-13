265aa1dda8ff57a679d25016d3223f2e6bbfd858
RenXie <yeyu0926@hotmail.com>
2018-09-13 23:04:52 +0800

    > 二进制文件比较

--- Foundation/diff.sh
+++ Foundation/diff.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-for line in `find ./src/main/java -type f`
+for line in `find ./00.src/01.before -type f`
 do
     after=${line/01\.before/02\.after}
-    before=`md5sum $line`
-    after=`md5sum $after`
 
-    before=${before:0:32}
-    after=${after:0:32}
+    # 方式1:二进制比较
+    cmp $line $after
 
-    if [ $before != $after ]
-    then
-        echo $(basename $line) >> 1
-        echo $before : $after >> 1
-    fi
+    # 方式2:md5比较
+    # before=`md5sum $line`
+    # after=`md5sum $after`
+
+    # before=${before:0:32}
+    # after=${after:0:32}
+
+    # if [ $before != $after ]
+    # then
+    #     echo $(basename $line) >> 1
+    #     echo $before : $after >> 1
+    # fi
 done
