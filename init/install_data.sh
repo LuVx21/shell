9d30ad4b1f41c4d94d887567bf73b4a8f27a9545
F.LuVx <yeyu0926@hotmail.com>
2019-09-23 16:13:04 +0800

    update

--- init/install_data.sh
+++ init/install_data.sh
@@ -4,19 +4,16 @@ wget http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-3.1.2/hadoo
 wget https://mirrors.tuna.tsinghua.edu.cn/apache/hive/hive-3.1.2/apache-hive-3.1.2-bin.tar.gz
 wget https://mirrors.tuna.tsinghua.edu.cn/apache/hbase/2.2.1/hbase-2.2.1-bin.tar.gz
 wget http://mirrors.tuna.tsinghua.edu.cn/apache/hbase/2.2.1/hbase-2.2.1-client-bin.tar.gz
-wget https://apache.org/dist/zookeeper/zookeeper-3.5.5/apache-zookeeper-3.5.5-bin.tar.gz
 
 mkdir temp
 tar -zxvf hadoop-3.1.2.tar.gz -C ./temp
 tar -zxvf apache-hive-3.1.2-bin.tar.gz -C ./temp
 tar -zxvf hbase-2.2.1-bin.tar.gz -C ./temp
 tar -zxvf hbase-2.2.1-client-bin.tar.gz -C ./temp
-tar -zxvf apache-zookeeper-3.5.5-bin.tar.gz -C ./temp
 
 cd temp;
-cd /usr/local/;sudo rm -fr hive zookeeper hadoop hbase hbase_client;cd -;
+cd /usr/local/;sudo rm -fr hive hadoop hbase hbase_client;cd -;
 sudo mv apache-hive-3.1.2-bin /usr/local/hive
-sudo mv apache-zookeeper-3.5.5-bin /usr/local/zookeeper
 sudo mv hadoop-3.1.2 /usr/local/hadoop
 sudo mv hbase-2.2.1 /usr/local/hbase
 sudo mv hbase-2.2.1-client /usr/local/hbase_client
