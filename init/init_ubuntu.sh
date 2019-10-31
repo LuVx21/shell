a5166ab895a6bee0333af8e5c290af5eb7189d73
F.LuVx <yeyu0926@hotmail.com>
2019-10-31 10:10:18 +0800

    update

--- init/init_ubuntu.sh
+++ init/init_ubuntu.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 sudo apt-get update
+
+# ssh
+ps -e | grep ssh
+/etc/init.d/ssh -start
+sudo apt-get install openssh-server
+
+# 防火墙
+apt install ufw
+sudo ufw enable
+sudo ufw enable deny
+sudo ufw status
+
+
 sudo apt-get install build-essential libtool gcc automake autoconf make openssl
