6d15038dc2cd86323f95bdb66304655e68cd9de7
F.LuVx <yeyu0926@hotmail.com>
2019-12-10 18:14:02 +0800

    update

--- init/gradle_install.sh
+++ init/gradle_install.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
-wget https://services.gradle.org/distributions/gradle-4.5.1-bin.zip
+version=5.6.4
+# https://services.gradle.org/distributions/gradle-4.5.1-bin.zip
+url=https://downloads.gradle-dn.com/distributions/gradle-$version-bin.zip
+filename=gradle-$version-bin.zip
+sudo wget -c $url -O $filename
 
-unzip -d /usr/local gradle-4.5.1-bin.zip
-mv /usr/local/gradle-4.5.1 /usr/local/gradle
+sudo mkdir -p /opt/install
+sudo unzip -d /opt/install $filename
+sudo ln -s /opt/install/gradle-$version /opt/gradle
+
+sudo rm -f $filename
