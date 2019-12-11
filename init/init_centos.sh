6ce4ffe159ad9833651ffe4563075bd3f36b6254
F.LuVx <yeyu0926@hotmail.com>
2019-12-11 16:24:51 +0800

    update

--- init/init_centos.sh
+++ init/init_centos.sh
@@ -20,3 +20,36 @@ sudo yum install cloc autojump tmux htop dos2unix
 # 安装 DNF
 yum install epel-release
 yum install dnf
+
+
+# 安装denyhosts
+wget http://jaist.dl.sourceforge.net/project/denyhosts/denyhosts/2.6/DenyHosts-2.6.tar.gz
+tar -zxvf DenyHosts-2.6.tar.gz
+cd DenyHosts-2.6
+python setup.py install
+# 进入安装目录
+cd /usr/share/denyhosts/
+# 创建配置文件
+cp denyhosts.cfg-dist denyhosts.cfg
+# 启动文件副本
+cp daemon-control-dist daemon-control
+# 创建软连接
+ln -s /usr/share/denyhosts/daemon-control /etc/init.d/denyhosts
+# 设置Denyhosts为开机自动启动
+# 添加denyhosts服务
+chkconfig --add denyhosts
+# 设置denyhosts在各等级为开启状态
+chkconfig denyhosts on
+# 查询是否设置成功
+chkconfig --list denyhosts
+# 启动denyhosts服务
+systemctl start denyhosts
+# 查询denyhosts服务状态
+systemctl status denyhosts
+# denyhosts的配置文件
+cat /usr/share/denyhosts/denyhosts.cfg
+# 白名单列表
+cat /etc/hosts.allow
+# 黑名单列表
+cat /etc/hosts.deny
+
