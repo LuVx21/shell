d4039374d3321ea28d04c3921ff8650612a364fc
F.LuVx <yeyu0926@hotmail.com>
2020-05-31 12:25:57 +0800

    update

--- init/jdk_install.sh
+++ init/jdk_install.sh
@@ -1,15 +1,26 @@
 #! /bin/bash
 
-
+# 删除
 yum list installed | grep java
 yum -y remove java-1.8.0-openjdk.x86_64
 wget -c +url 
 
 
+# https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
+
 # http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.tar.gz
-mkdir -p /usr/local/java
-tar -zxvf jdk-8u162-linux-x64.tar.gz -C /usr/local/java
+# wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.tar.gz
+# mkdir -p /usr/local/java
+# tar -zxvf jdk-8u162-linux-x64.tar.gz -C /usr/local/java
 
 
+sudo mkdir -p /opt/install
+filename=$1
+sudo tar -zxvf $filename -C /opt/install/
+
+arr=$(ls -l /opt/install | grep jdk | awk '{print $9}')
+for i in ${arr[*]}
+do
+    dirnm=$i
+done
 
--- init/mysql_install.sh
+++ init/mysql_install.sh
@@ -1,34 +1,22 @@
 #! /bin/bash
 
-# https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-server_5.7.21-1ubuntu16.04_amd64.deb-bundle.tar
-
-groupadd mysql
-useradd -r -g mysql mysql
-
-mkdir -p /usr/local/mysql
-tar -xvf mysql-server_5.7.21-1ubuntu16.04_amd64.deb-bundle.tar
-tar -zxvf mysql-server_5.7.21-1ubuntu16.04_amd64.deb-bundle.tar.gz -C /usr/local/mysql
-
-# sudo apt-get install mysql-server
-# sudo apt-get install mysql-client
-# sudo apt-get install libmysqlclient-dev
-# sudo service mysql start
+#↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
 # CentOS
 wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
 yum localinstall mysql57-community-release-el7-11.noarch.rpm
 
+# 确认仓库
 # ll /etc/yum.repos.d
 # yum repolist enabled | grep "mysql.*-community.*"
 
 yum install mysql-community-server
 systemctl start mysqld
 systemctl status mysqld
+# 其他配置
 # systemctl stop mysqld
-
-# 开机启动
-systemctl enable mysqld
-systemctl daemon-reload
+# systemctl disenable mysqld
+# systemctl daemon-reload
 
 # 修改密码
 grep 'temporary password' /var/log/mysqld.log
@@ -43,9 +31,34 @@ grep 'temporary password' /var/log/mysqld.log
 # grant all privileges on *.* to 'root'@'%' identified by '1121' with grant option;
 # flush privileges;
 
-## 编码
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+
+# 自定义安装
+#########################################################################
+
+# wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-server_5.7.21-1ubuntu16.04_amd64.deb-bundle.tar
+
+# # groupadd mysql
+# # useradd -r -g mysql mysql
+
+# mkdir -p /usr/local/mysql
+# tar -xvf mysql-server_5.7.21-1ubuntu16.04_amd64.deb-bundle.tar
+# tar -zxvf mysql-server_5.7.21-1ubuntu16.04_amd64.deb-bundle.tar.gz -C /usr/local/mysql
+
+#########################################################################
+
+## 一些配置: 编码
 # show variables like 'char%'
 # vim /etc/my.cnf # [mysqld]
 # character-set-server=utf8
 # character-set-database=utf8
 # default-character-set=utf8
+
+# Ubuntu
+###################################################
+# sudo apt-get install mysql-server
+# sudo apt-get install mysql-client
+# sudo apt-get install libmysqlclient-dev
+# sudo service mysql start
+###################################################
