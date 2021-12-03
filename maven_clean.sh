f356c2b1f48fc2eefbd3ec25db8e10a152b41db2
F.LuVx <yeyu0926@hotmail.com>
2021-12-03 22:36:38 +0800

    update

--- maven_clean.sh
+++ maven_clean.sh
@@ -2,25 +2,22 @@
 
 # rm ./1.txt ./2.txt
 
-find ~/.m2 -iname '*.pom' | while read pom;
-do
-    parent=`dirname "$pom"`;
-    echo `dirname "$parent"` >> 1.txt;
-done;
+find ~/.m2 -iname '*.pom' | while read pom; do
+  parent=$(dirname "$pom")
+  echo $(dirname "$parent") >>1.txt
+done
 
-cat 1.txt | uniq -d > 2.txt
+cat 1.txt | uniq -d >2.txt
 
-for line in `cat 2.txt`;
-do
-    last=`ls $line | tail -1`;
-    for file in `ls $line`;
-    do
-        # echo $file;
-        if [ "$file" != "$last" ]; then
-            # echo $line/$file;
-            rm -rf $line/$file;
-        fi;
-    done;
-done;
+for line in $(cat 2.txt); do
+  last=$(ls $line | tail -1)
+  for file in $(ls $line); do
+    # echo $file;
+    if [ "$file" != "$last" ]; then
+      # echo $line/$file;
+      rm -rf $line/$file
+    fi
+  done
+done
 
 rm ./1.txt ./2.txt
