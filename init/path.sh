b09730de71e3e8680aacfb60c3d4c42486e5282e
Ren,Xie <LuVx21@users.noreply.github.com>
2022-06-15 16:09:19 +0000

    update

--- init/path.sh
+++ init/path.sh
@@ -2,14 +2,14 @@
 opt_home=/opt
 
 # Java
-export JAVA_HOME=$opt_home/java/Contents/Home
-export JRE_HOME=$JAVA_HOME/jre
-export CLASSPATH=$JAVA_HOME/lib:$JRE_HOME/lib
-export PATH=$PATH:$JAVA_HOME/bin
+# export JAVA_HOME=$opt_home/java/Contents/Home
+# export JRE_HOME=$JAVA_HOME/jre
+# export CLASSPATH=$JAVA_HOME/lib:$JRE_HOME/lib
+# export PATH=$PATH:$JAVA_HOME/bin
 
 # Maven
-export M2_HOME=$opt_home/maven
-export PATH=$PATH:$M2_HOME/bin
+# export M2_HOME=$opt_home/maven
+# export PATH=$PATH:$M2_HOME/bin
 
 # Python
 # export PATH=$PATH:$opt_home/python3/bin
@@ -19,15 +19,6 @@ export PATH=$PATH:$M2_HOME/bin
 
 # MongoDB
 # export PATH=$PATH:$opt_home/mongodb/bin
-
-# Tomcat
-# export CATALINA_HOME=$opt_home/tomcat9
-# export PATH=$PATH:$CATALINA_HOME/bin
-
-# Spring
-# export SPRING_HOME=$opt_home/spring-1.5.9.RELEASE
-# export SPRING=$SPRING_HOME/bin
-# export PATH=$SPRING:$PATH
 
 # Gradle
 # export GRADLE_HOME=$opt_home/gradle
@@ -40,19 +31,12 @@ export PATH=$PATH:$M2_HOME/bin
 # Oracle
 # export PATH=$PATH:$opt_home/instantclient_12_1
 
-# phantomjs
-# export PATH=$PATH:$opt_home/phantomjs-2.1.1/bin
-
 # NodeJS
 # export PATH=$PATH:$opt_home/nodejs/bin
 
 # Hadoop
 # export HADOOP_HOME=$opt_home/hadoop
 # export PATH=$PATH:$HADOOP_HOME/bin
-
-# flutter
-# export FLUTTER_HOME=$opt_home/flutter
-# export PATH=$PATH:$FLUTTER_HOME/bin
 
 # RabbitMQ
 # export RABBITMQ_HOME=$opt_home/rabbitmq
