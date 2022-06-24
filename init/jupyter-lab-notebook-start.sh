78ff0a81d1e9f4f888dafee28d8c22b3d6235e86
F.LuVx <yeyu0926@hotmail.com>
2022-06-24 10:10:07 +0800

    update

--- init/jupyter-lab-notebook-start.sh
+++ init/jupyter-lab-notebook-start.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-# pip install --upgrade pip
-# pip install ipython jupyter jupyterlab
+pip install --upgrade pip
+pip install ipython \
+# jupyter \
+jupyterlab
+
+# Java内核
+wget https://github.com/SpencerPark/IJava/releases/download/v1.3.0/ijava-1.3.0.zip
+unzip ijava-1.3.0.zip -d ./ijava
+cd ijava && python install.py --sys-prefix && cd -
+
+# kotlin内核
+pip install kotlin-jupyter-kernel
 
 # go内核
 # https://github.com/gopherdata/gophernotes
-# env GO111MODULE=off go get -d -u github.com/gopherdata/gophernotes
-# cd "$(go env GOPATH)"/src/github.com/gopherdata/gophernotes
-# env GO111MODULE=on go install
-# mkdir -p ~/.local/share/jupyter/kernels/gophernotes
-# cp kernel/* ~/.local/share/jupyter/kernels/gophernotes
-# cd ~/.local/share/jupyter/kernels/gophernotes
-# chmod +w ./kernel.json # in case copied kernel.json has no write permission
-# sed "s|gophernotes|$(go env GOPATH)/bin/gophernotes|" < kernel.json.in > kernel.json
-
-# kotlin内核
-# pip install kotlin-jupyter-kernel
-
-# Java内核
-# wget https://github.com/SpencerPark/IJava/releases/download/v1.3.0/ijava-1.3.0.zip
-# unzip ijava-1.3.0.zip -d ./ijava
-# cd ijava
-# python install.py --sys-prefix
+env GO111MODULE=off go get -d -u github.com/gopherdata/gophernotes
+cd "$(go env GOPATH)"/src/github.com/gopherdata/gophernotes
+env GO111MODULE=on go install
+mkdir -p ~/.local/share/jupyter/kernels/gophernotes
+cp kernel/* ~/.local/share/jupyter/kernels/gophernotes
+cd ~/.local/share/jupyter/kernels/gophernotes
+chmod +w ./kernel.json # in case copied kernel.json has no write permission
+sed "s|gophernotes|$(go env GOPATH)/bin/gophernotes|" < kernel.json.in > kernel.json
 
 # 插件
 # pip install jupyter_contrib_nbextensions
@@ -32,9 +33,9 @@
 # pip install autopep8
 
 # ------------------------------jupyter notebook-------------------------------------
-export PASSWORD=xxxx
-nohup jupyter notebook --ip 0.0.0.0 --port 58081 --no-browser --allow-root --notebook-dir ~/code/doc > out.file 2>&1 &
-echo -e "\r\nStarting..."
+# export PASSWORD=xxxx
+# nohup jupyter notebook --ip 0.0.0.0 --port 58081 --no-browser --allow-root --notebook-dir ~/code/doc > out.file 2>&1 &
+# echo -e "\r\nStarting..."
 
 # ------------------------------jupyter lab-------------------------------------
 # ipython
