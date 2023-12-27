d1da24628de74b8afa3a3ea2242435b846460848
F.LuVx <yeyu0926@hotmail.com>
2023-12-27 17:45:24 +0800

    delete

--- init/install_rabbitmq.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo yum install erlang
-wget https://dl.bintray.com/rabbitmq/all/rabbitmq-server/3.6.14/rabbitmq-server-3.6.14-1.el7.noarch.rpm
-sudo yum localinstall rabbitmq-server-3.6.14-1.el7.noarch.rpm
--- init/install_zookeeper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget http://mirrors.tuna.tsinghua.edu.cn/apache/zookeeper/zookeeper-3.4.14/zookeeper-3.4.14.tar.gz
-sudo tar -zxvf zookeeper-3.4.14.tar.gz -C /usr/local/
-sudo mv /usr/local/zookeeper-3.4.14 /usr/local/zookeeper
