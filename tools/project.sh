1dad4ea648c795d00d5e4be4eeb13aad8a09ed52
F.LuVx <yeyu0926@hotmail.com>
2024-01-09 23:41:52 +0800

    update

---
+++ tools/project.sh
@@ -0,0 +1,35 @@
+repository_name=coding-parent
+mkdir -p ./$repository_name/git/
+
+cat > ./$repository_name/git/config <<- EOF
+[user]
+    email = yeyu0926@hotmail.com
+    name = F.LuVx
+[core]
+    repositoryformatversion = 0
+    filemode = false
+    bare = false
+    logallrefupdates = true
+    symlinks = false
+    ignorecase = true
+    autocrlf = input
+    safecrlf = true
+[remote "origin"]
+    url = git@e.coding.net:LuVx21/code/${repository_name}
+    url = git@github.com:LuVx21/${repository_name}
+    url = git@gitee.com:LuVx/${repository_name}
+    fetch = +refs/heads/*:refs/remotes/origin/*
+[remote "coding"]
+    url = git@e.coding.net:LuVx21/code/${repository_name}
+    fetch = +refs/heads/*:refs/remotes/origin/*
+[remote "github"]
+    url = git@github.com:LuVx21/${repository_name}
+    fetch = +refs/heads/*:refs/remotes/origin/*
+[remote "gitee"]
+    url = git@gitee.com:LuVx/${repository_name}
+    fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+    remote = origin
+    merge = refs/heads/master
+
+EOF
