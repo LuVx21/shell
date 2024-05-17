7e756b42f929700b144b55d936fd75e521454931
F.LuVx <yeyu0926@hotmail.com>
2024-05-18 01:47:01 +0800

    update

--- bin/source/daily.sh
+++ bin/source/daily.sh
@@ -4,13 +4,14 @@ function daily-fn {
     echo "copy....."
     cp $HOME/Library/Application\ Support/Microsoft\ Edge\ Dev/Default/Cookies $HOME/data/sqlite
 
+    echo "delete......"
+    rm ~/docker/freshrss/data/cache/*.spc
+
     echo "brew....."
     brew update && brew upgrade && brew cu -af
 
     echo "brew clean"
     brew cleanup && brew autoremove
-
-    echo "delete......"
     rm -r $HOME/Library/Caches/Homebrew/downloads/*
 
     omz update &
